7edccd6556fb437c0a062d97979a46f476e55f7c
Rajiv Ranjan <51155706+rajivranjan89@users.noreply.github.com>
2023-01-29 10:30:52 +0530

    Update query_metadata_json.py

--- Code/query_metadata_json.py
+++ Code/query_metadata_json.py
@@ -25,3 +25,15 @@ vm = compute_client.virtual_machines.get(
 
 # Convert the response to JSON and print it
 print(json.dumps(vm.as_dict(), indent=2))
+
+
+
+# -------------------------------------------------------
+
+# Instance Metadata service provides information regarding your running virtual machine instances. 
+# This information can be used to manage and configure your instances on Azure. 
+# Azure's instance metadata service is a RESTful endpoint available to all IaaS VMs created via new Azure Resource Manager. 
+
+### Retrieving instance metadata
+
+Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -NoProxy -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | ConvertTo-Json -Depth 64
