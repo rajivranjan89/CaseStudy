0b02e857a639a40404583df8fe7df529cea992c6
Rajiv Ranjan <51155706+rajivranjan89@users.noreply.github.com>
2023-01-29 10:35:57 +0530

    Create local.tf

---
+++ local.tf
@@ -0,0 +1,3 @@
+subnet_id = data.terraform_remote_state.gateway_primary_azure.outputs.subnet_id
+nsg_id = data.terraform_remote_state.gateway_primary_azure.outputs.nsg_id
+nsg_ame = data.terraform_remote_state.gateway_primary_azure.outputs.testnsg_name
