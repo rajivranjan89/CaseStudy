2fd8e2c867666329837b447b57c435f9e4d0f027
Rajiv Ranjan <51155706+rajivranjan89@users.noreply.github.com>
2023-01-29 10:45:12 +0530

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals { 
   subnet_id = data.terraform_remote_state.gateway_primary_azure.outputs.subnet_id
   nsg_id = data.terraform_remote_state.gateway_primary_azure.outputs.nsg_id
-  nsg_ame = data.terraform_remote_state.gateway_primary_azure.outputs.testnsg_name
+  nsg_name = data.terraform_remote_state.gateway_primary_azure.outputs.testnsg_name
 }
