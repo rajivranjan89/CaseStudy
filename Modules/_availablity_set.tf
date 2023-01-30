a2a950cc7b7dca77ea03acd44a6957636e660f88
Rajiv Ranjan <51155706+rajivranjan89@users.noreply.github.com>
2023-01-30 10:02:34 +0530

    Update _availablity_set.tf

--- Modules/_availablity_set.tf
+++ Modules/_availablity_set.tf
@@ -2,8 +2,6 @@ resource "azurerm_availablity_set" "availablity_set" {
   name = "web-AS"
   location = var.primary_region
   resource_group_name = var.resource_group_name
-  platform_update_domain_count = var.as_platform_update_domain_count
-  platform_fault_domain_count = var.as_platform_fault_domain_count
   managed = true
   
  tag = {
@@ -14,8 +12,6 @@ resource "azurerm_availablity_set" "availablity_set" {
   name = "sql-AS"
   location = var.primary_region
   resource_group_name = var.resource_group_name
-  platform_update_domain_count = var.as_platform_update_domain_count
-  platform_fault_domain_count = var.as_platform_fault_domain_count
   managed = true
   
  tag = {
