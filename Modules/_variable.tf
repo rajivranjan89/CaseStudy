e3d540a0076e11bf22c3f7a020acb9124e90dd14
Rajiv Ranjan <51155706+rajivranjan89@users.noreply.github.com>
2023-01-30 09:54:27 +0530

    Update _variable.tf

--- Modules/_variable.tf
+++ Modules/_variable.tf
@@ -1,1 +1,45 @@
+variable "web_vms" {
+  description = "attribute for web server"
+  type = map(string)
+  
+  default = {
+    count = 1
+    hostname = ""
+    vm_size = ""
+    
+  }
+}
+variable "sql_vms" {
+  description = "attribute for web server"
+  type = map(string)
+  
+  default = {
+    count = 1
+    hostname = ""
+    vm_size = ""
+    
+  }
+}
+
+variable "resource_group_name" {
+  description = "name of RG"
+}
+
+variable "primary_region" {
+  description = "Name of Primary region"
+}
+
+variable "vm_default_admin_username" {
+  description = "name of login user"
+}
+
+variable "vm_storage_os_disk_caching" {
+}
+
+variable "managed_disk_type" {
+}
+
+variable "ag_environment_type" {
+}
+
 
